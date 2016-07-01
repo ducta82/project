758cc4edfea3438acab2334a1be7df40af9fceda
ducta82 <ducta82@gmail.com>
2016-07-01 17:57:53 +0700

    font+header

--- procfo.com.au/index.php
+++ procfo.com.au/index.php
@@ -7,7 +7,9 @@
 		<title>Title Page</title>
 
 		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<link rel="stylesheet" href="/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/style.css">
+		<link rel="stylesheet" href="/css/font-awesome.min.css">
 
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -17,12 +19,89 @@
 		<![endif]-->
 	</head>
 	<body>
-		<h1 class="text-center">Hello World</h1>
-
+		<section class="wapper">
+			<div class="container-fluid">
+				<header id="site-header">
+					<div class="container-header">
+						<div class="content">
+							<div class="box-btn">
+								<a class="btn btn-1" href="#">Call Now (08) 6311 9799</a>
+								<a class="btn btn-2" href="#">Book a Free Consultation</a>
+							</div>
+						</div>
+					</div>
+					<nav class="navbar navbar-default" role="navigation">
+							<div class="content">
+								<!-- Brand and toggle get grouped for better mobile display -->
+								<div class="navbar-header">
+									<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+										<span class="sr-only">Toggle navigation</span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+									</button>
+								</div>
+						
+								<!-- Collect the nav links, forms, and other content for toggling -->
+								<div class="collapse navbar-collapse navbar-ex1-collapse">
+									<ul class="nav navbar-nav navbar-right">
+										<li><a href="#">home</a></li>
+										<li><a href="#">Book Keeping Service</a></li>
+										<li><a href="#">Business & Strategic Planning</a></li>
+										<li><a href="#">Virtual CFO</a></li>
+										<li><a href="#">Blog</a></li>
+										<li><a href="#">Testimonials</a></li>
+										<li><a href="#">About</a></li>
+										<li><a href="#">Contact</a></li>
+									</ul>
+								</div><!-- /.navbar-collapse -->
+							</div>
+						</nav>	
+				</header>	
+				<section id="site-content">
+					<div class="container-site-content container content">
+						<article class="article-content">
+							<div class="box-post-content">
+								<h2>procfo - what we do</h2>
+								<div class="post-content">
+									<p>Our team of professional accountants and financial officers will keep your business running smoothly. Our fresh eyes can easily identify any areas needing addressing.</p>
+									<p>The process is simple. Your financial reports are uploaded through XERO by either you or your current accountant. proCFO then accesses your financial metrics and begins identifying your business’s strengths and weaknesses, and identifies value-adding opportunities, a service which your everyday accountant will not provide</p>
+									<strong><p>Some key areas we address include:</p></strong>
+									<ol>
+										<li>Budget preparation and monitoring</li>
+									   <li>Customised business dashboards</li>
+									   <li>Annual strategy planning sessions</li>
+									   <li>Market research with key customers</li>
+									   <li>Human resource management</li>
+									   <li>Identifying key profit opportunities</li>
+									   <li>Regular performance reports prepared for owners/managers</li>
+									</ol>
+									<p>With proCFO you have all the benefits of a CFO without the significant cost of a full-time employee. We provide you with accurate, understandable and up to date financial data, giving you the confidence to make the right decisions for your business. We are the market leaders in virtual CFOs, so let us take the stress out of your bookkeeping. Using our innovative cloud based system, we will bring you the latest in financial administration services delivered with flexibility and a custom designed approach.</p>
+									<p>Our Services:
+									   » Book Keeping Service   
+									   » Business & Strategic Planning, Business Management & Advisory   
+									   » Virtual CFO   </p>		
+								</div>	
+							</div>
+						</article>
+						<aside class="widget">
+							<div class="box-widget">
+								
+							</div>
+						</aside>
+					</div>
+				</section>
+				<footer id="site-footer">
+					<div id="footer-content" class="content">
+						
+					</div>
+				</footer>
+			</div>
+		</section>
 		<!-- jQuery -->
-		<script src="//code.jquery.com/jquery.js"></script>
+		<script src="/js/jquery-1.12.4.min.js"></script>
 		<!-- Bootstrap JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<script src="/js/bootstrap.min.js"></script>
 		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
  		<script src="Hello World"></script>
 	</body>
